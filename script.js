5e266e76f8852e8c1fe69cc7d45ba97aad4167ee
Mahima Meghana <mahimameghana80083@gmail.com>
2026-06-02 16:37:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
-function addTask()
-{
-  let task=document.getElementById("TI").value;
-  let li=document.createElement("li");
-  li.innerText=task;
-  document.getElementById("taskList").appendChild(li);
-  document.getElementById("TI").value="";
+let tasks = [];
+
+// Load tasks when page opens
+window.onload = function() {
+
+    let savedTasks = localStorage.getItem("tasks");
+
+    if(savedTasks) {
+        tasks = JSON.parse(savedTasks);
+
+        for(let task of tasks) {
+            displayTask(task);
+        }
+    }
+};
+
+// Add Task
+function addTask() {
+
+    let task = document.getElementById("TI").value;
+
+    if(task === "") {
+        return;
+    }
+
+    tasks.push(task);
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    displayTask(task);
+
+    document.getElementById("TI").value = "";
+}
+
+// Display Task
+function displayTask(task) {
+
+    let li = document.createElement("li");
+
+    li.innerText = task;
+
+    document.getElementById("taskList").appendChild(li);
 }
