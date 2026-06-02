47c603beb9f037eda66a977e23b3c49bb5fd08c1
Mahima Meghana <mahimameghana80083@gmail.com>
2026-06-02 17:27:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let tasks = [];
 
 // Load tasks when page opens
-window.onload = function() {
+window.onload = function () {
 
     let savedTasks = localStorage.getItem("tasks");
 
-    if(savedTasks) {
+    if (savedTasks) {
         tasks = JSON.parse(savedTasks);
 
         tasks.forEach((task, i) => {
@@ -19,81 +19,19 @@ function addTask() {
 
     let task = document.getElementById("TI").value;
 
-    if(task === "") {
+    if (task === "") {
         return;
     }
 
     tasks.push({
-        text:task,
-        completed:false
+        text: task,
+        completed: false
     });
 
     localStorage.setItem("tasks", JSON.stringify(tasks));
 
-    let tasks = [];
+    displayTask(tasks[tasks.length - 1], tasks.length - 1);
 
-// Load tasks when page opens
-window.onload = function() {
-
-    let savedTasks = localStorage.getItem("tasks");
-
-    if(savedTasks) {
-        tasks = JSON.parse(savedTasks);
-
-        tasks.forEach((task, i) => {
-            displayTask(task, i);
-        });
-    }
-};
-
-// Add Task
-function addTask() {
-
-    let task = document.getElementById("TI").value;
-
-    if(task === "") {
-        return;
-    }
-
-    tasks.push({
-        text:task,
-        completed:false
-    });
-
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-
-    let tasks = [];
-
-// Load tasks when page opens
-window.onload = function() {
-
-    let savedTasks = localStorage.getItem("tasks");
-
-    if(savedTasks) {
-        tasks = JSON.parse(savedTasks);
-
-        tasks.forEach((task, i) => {
-            displayTask(task, i);
-        });
-    }
-};
-
-// Add Task
-function addTask() {
-
-    let task = document.getElementById("TI").value;
-
-    if(task === "") {
-        return;
-    }
-
-    tasks.push({
-        text:task,
-        completed:false
-    });
-
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-    displayTask(tasks[tasks.length-1], tasks.length-1);
     document.getElementById("TI").value = "";
 }
 
@@ -103,14 +41,24 @@ function displayTask(task, index) {
     let li = document.createElement("li");
 
     li.innerText =
-    (task.completed ? "☑ " : "☐ ")
-    + task.text + " ";
+        (task.completed ? "☑ " : "☐ ")
+        + task.text + " ";
+
+    let completeBtn = document.createElement("button");
+
+    completeBtn.innerText = "Complete";
+
+    completeBtn.onclick = function () {
+        toggleComplete(index);
+    };
+
+    li.appendChild(completeBtn);
 
     let deleteBtn = document.createElement("button");
 
     deleteBtn.innerText = "Delete";
 
-    deleteBtn.onclick = function() {
+    deleteBtn.onclick = function () {
         deleteTask(index);
     };
 
@@ -118,16 +66,8 @@ function displayTask(task, index) {
 
     document.getElementById("taskList").appendChild(li);
 }
-let completeBtn = document.createElement("button");
 
-completeBtn.innerText = "Complete";
-
-completeBtn.onclick = function() {
-    toggleComplete(index);
-};
-
-li.appendChild(completeBtn);
-//delete function
+// Delete Task
 function deleteTask(index) {
 
     tasks.splice(index, 1);
@@ -140,133 +80,21 @@ function deleteTask(index) {
         displayTask(task, i);
     });
 }
-function toggleComplete(index){
+
+// Complete Task
+function toggleComplete(index) {
 
     tasks[index].completed =
-    !tasks[index].completed;
+        !tasks[index].completed;
 
     localStorage.setItem(
         "tasks",
         JSON.stringify(tasks)
     );
-
-    document.getElementById(
-        "taskList"
-    ).innerHTML = "";
-
-    tasks.forEach((task,i)=>{
-        displayTask(task,i);
-    });
-}
-
-    document.getElementById("TI").value = "";
-}
-
-// Display Task
-function displayTask(task, index) {
-
-    let li = document.createElement("li");
-
-    li.innerText =
-    (task.completed ? "☑ " : "☐ ")
-    + task.text + " ";
-
-    let deleteBtn = document.createElement("button");
-
-    deleteBtn.innerText = "Delete";
-
-    deleteBtn.onclick = function() {
-        deleteTask(index);
-    };
-
-    li.appendChild(deleteBtn);
-
-    document.getElementById("taskList").appendChild(li);
-}
-//delete function
-function deleteTask(index) {
-
-    tasks.splice(index, 1);
-
-    localStorage.setItem("tasks", JSON.stringify(tasks));
 
     document.getElementById("taskList").innerHTML = "";
 
     tasks.forEach((task, i) => {
         displayTask(task, i);
-    });
-}
-function toggleComplete(index){
-
-    tasks[index].completed =
-    !tasks[index].completed;
-
-    localStorage.setItem(
-        "tasks",
-        JSON.stringify(tasks)
-    );
-
-    document.getElementById(
-        "taskList"
-    ).innerHTML = "";
-
-    tasks.forEach((task,i)=>{
-        displayTask(task,i);
-    });
-}
-
-    document.getElementById("TI").value = "";
-}
-
-// Display Task
-function displayTask(task, index) {
-
-    let li = document.createElement("li");
-
-    li.innerText =
-    (task.completed ? "☑ " : "☐ ")
-    + task.text + " ";
-
-    let deleteBtn = document.createElement("button");
-
-    deleteBtn.innerText = "Delete";
-
-    deleteBtn.onclick = function() {
-        deleteTask(index);
-    };
-
-    li.appendChild(deleteBtn);
-
-    document.getElementById("taskList").appendChild(li);
-}
-//delete function
-function deleteTask(index) {
-
-    tasks.splice(index, 1);
-
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-
-    document.getElementById("taskList").innerHTML = "";
-
-    tasks.forEach((task, i) => {
-        displayTask(task, i);
-    });
-}
-function toggleComplete(index){
-
-    tasks[index].completed =
-    !tasks[index].completed;
-
-    localStorage.setItem(
-        "tasks",
-        JSON.stringify(tasks)
-    );
-
-    document.getElementById(
-        "taskList"
-    ).innerHTML = "";
-
-    tasks.forEach((task,i)=>{
-        displayTask(task,i);
     });
 }
